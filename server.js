ca06aa8f939aad79eb0ca9efc331852414afd6cb
EugeneFi1 <51033432+EugeneFi1@users.noreply.github.com>
2021-03-29 23:23:20 +0300

    refactor

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
+//const MongoClient = require('mongodb').MongoClient;
+//const bodyParser = require('body-parser');
 const app = express();
 
 const port = 8000;
-app.listen(port, () => {console.log('We are live on ' + port);});
+app.listen(port, () => {
+    console.log('We are live on ' + port);
+});
 
-app.get("/get",function (req,res){
-    const hello = {name:"hello world"};
+app.get("/get", function (req, res) {
+    const hello = {name: "hello world"};
     res.send(hello);
+});
+app.get("/get-all", function (req, res) {
+    res.send({name: "get-all"});
 });
